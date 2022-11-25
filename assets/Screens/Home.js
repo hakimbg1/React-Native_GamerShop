c95a5f2b8c8e0e1caf11f2cb0c7a235f7611ed46
hakimbg1 <74165701+hakimbg1@users.noreply.github.com>
2022-11-26 00:17:00 +0100

    Update Home.js

--- assets/Screens/Home.js
+++ assets/Screens/Home.js
@@ -6,7 +6,6 @@ import SearchGoogle from '../../Components/SearchGoogle';
 import ShopItems, { localShops } from '../../Components/ShopItems';
 
 
-const YELP_API_KEY = "WHc5gLsDspwuBBEL3-J8XSLPx_VnuvmT5b63MAF3z_s9paiBWm7Nd3yURR00pLvzimY9XeT9rkYRRA-qR-p2OpYUzrnb9RodYZPGTJp-r4l_lZ8dfICO8czq5OJoY3Yx"; //API(database for my Shops!) that i found in the internet but it wont help me cuz i can not find a search for specifique store (for my case gamming)
 
 export default function Home({navigation}){
     const [ShopData, setShopData] = React.useState(localShops)
